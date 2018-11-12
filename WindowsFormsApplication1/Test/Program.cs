994457892cea233052c0953c181400c601699a1f
Maxime Adolphe <madolphe@ensc.fr>
2018-11-12 11:38:11 +0100

    try bdd connection

---
+++ WindowsFormsApplication1/Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Test
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            NHibernate.Cfg.Configuration cfg = new NHibernate.Cfg.Configuration();
+            cfg.Configure();
+            new SchemaExport(cfg).Execute(true, true, false);
+
+
+
+        }
+    }
+}
